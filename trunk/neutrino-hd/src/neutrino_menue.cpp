4f890c25b86118c1b1a5e34ae1bbeab0edb329ba
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2012-07-01 12:21:42 +0000

    fix typo

--- trunk/neutrino-hd/src/neutrino_menue.cpp
+++ trunk/neutrino-hd/src/neutrino_menue.cpp
@@ -756,7 +756,7 @@ void CNeutrinoApp::InitServiceSettings(CMenuWidget &service, CMenuWidget & Tuner
 		TunerSetup.addItem(GenericMenuBack);
 		TunerSetup.addItem( new CMenuSeparator(CMenuSeparator::LINE) );
 		
-		TunerSetup.addItem(new CMenuForwarderNonLocalized(" Tuner Mode", false, "independant", NULL ));
+		TunerSetup.addItem(new CMenuForwarderNonLocalized("Tuner Mode", false, "independant", NULL ));
 		
 		TunerSetup.addItem( new CMenuSeparator(CMenuSeparator::LINE) );
 		
