f6a4d2424e4eeb228e884f43dc51b09c8fbd1455
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2012-07-12 18:02:55 +0000

    dummy value to shut up compiler (?)

--- trunk/neutrino-hd/lib/liblcddisplay/lcddisplay.cpp
+++ trunk/neutrino-hd/lib/liblcddisplay/lcddisplay.cpp
@@ -43,7 +43,7 @@ CLCDDisplay::CLCDDisplay()
 {
 	paused=0;
 	available = false;
-#ifndef HAVE_GENERIC_HARDWARE
+	
 	//open device
 	if ((fd = open(LCD_DEVICE, O_RDWR)) < 0)
 	{
@@ -52,26 +52,21 @@ CLCDDisplay::CLCDDisplay()
 	}
 
 	//clear the display
-	if ( ioctl(fd,LCD_IOCTL_CLEAR) < 0)
+	if ( ioctl(fd, LCD_IOCTL_CLEAR) < 0)
 	{
 		perror("clear failed");
 		return;
 	}
 	
 	//graphic (binary) mode 
-#ifdef HAVE_TRIPLEDRAGON
-	if (ioctl(fd, IOC_LCD_WRMODE, LCD_MODE_BIN) < 0)
-#else
 	int i=LCD_MODE_BIN;
-	if ( ioctl(fd,LCD_IOCTL_ASC_MODE,&i) < 0 )
-#endif
+	if ( ioctl(fd, LCD_IOCTL_ASC_MODE, &i) < 0 )
 	{
 		perror("graphic mode failed");
 		return;
 	}
 
 	available = true;
-#endif
 	iconBasePath = "";
 }
 
@@ -92,26 +87,21 @@ void CLCDDisplay::pause()
 
 void CLCDDisplay::resume()
 {
-#ifndef HAVE_GENERIC_HARDWARE
 	//clear the display
-	if ( ioctl(fd,LCD_IOCTL_CLEAR) < 0)
+	if ( ioctl(fd, LCD_IOCTL_CLEAR) < 0)
 	{
 		perror("clear failed");
 		return;
 	}
 	
 	//graphic (binary) mode 
-#ifdef HAVE_TRIPLEDRAGON
-	if (ioctl(fd, IOC_LCD_WRMODE, LCD_MODE_BIN) < 0)
-#else
 	int i=LCD_MODE_BIN;
 	if ( ioctl(fd,LCD_IOCTL_ASC_MODE,&i) < 0 )
-#endif
 	{
 		perror("graphic mode failed");
 		return;
 	}
-#endif
+	
 	paused = 0;
 }
 
@@ -130,48 +120,56 @@ void CLCDDisplay::convert_data()
 	   must shift the whole buffer one pixel to the right. This is wrong for
 	   the column 127 (rightmost), which is shifted up 8 lines.
 	 */
-	for (y = 0; y < LCD_LINES; y++) {
-		for (x = 0; x < LCD_STRIDE; x++) {
+	for (y = 0; y < LCD_LINES; y++) 
+	{
+		for (x = 0; x < LCD_STRIDE; x++) 
+		{
 			pix = 0;
 			bit = 0x80;
 
-			for (xx = x * 8; xx < x * 8 + 8; xx++, bit >>= 1) {
+			for (xx = x * 8; xx < x * 8 + 8; xx++, bit >>= 1) 
+			{
 				xp = xx - 1;		/* shift the whole buffer one pixel to the right */
 				yp = y;
-				if (xp < 0) {		/* rightmost column (column 127) */
+				if (xp < 0) 
+				{		/* rightmost column (column 127) */
 					xp += LCD_COLS;	/* wraparound */
 					yp -= 8;	/* shift down 8 lines */
 					if (yp < 0)	/* wraparound */
 						yp += LCD_LINES;
 				}
+				
 				if (raw[yp][xp] == 1)
 					pix |= bit;
 			}
 /* I was chasing this one for quite some time, so check it for now */
 #if 1
-		if (y * LCD_STRIDE + x > LCD_BUFFER_SIZE)
-			fprintf(stderr, "%s: y (%d) * LCD_STRIDE (%d) + x (%d) (== %d) > LCD_BUFFER_SIZE (%d)\n",
+			if (y * LCD_STRIDE + x > LCD_BUFFER_SIZE)
+				fprintf(stderr, "%s: y (%d) * LCD_STRIDE (%d) + x (%d) (== %d) > LCD_BUFFER_SIZE (%d)\n",
 				__FUNCTION__, y, LCD_STRIDE, x, y*LCD_STRIDE+x, LCD_BUFFER_SIZE);
-		else
+			else
 #endif
-			lcd[y * LCD_STRIDE + x] = pix;
+				lcd[y * LCD_STRIDE + x] = pix;
 		}
 	}
 
 #if 0
-/* alternative solution, just ignore the rightmost column (which would be the
-   MSB of the first byte */
-	for (y=0; y<64; y++){
-		for (x=0; x<(128/8); x++){
+	/* alternative solution, just ignore the rightmost column (which would be the MSB of the first byte */
+	for (y=0; y<64; y++)
+	{
+		for (x=0; x<(128/8); x++)
+		{
 			int pix=0, start = 0;
 			unsigned char bit = 0x80;
 			int offset=(y*128)+x*8;
-			if (x == 0) {	/* first column, skip MSB */
+			if (x == 0) 
+			{	/* first column, skip MSB */
 				start = 1;
 				bit = 0x40;
 			} else
 				offset--;
-			for (yy=start; yy<8; yy++, bit >>=1) {
+			for (yy=start; yy<8; yy++, bit >>=1) 
+			{
 				pix|=(_buffer[offset++]>=108)?bit:0;
 			}
 			raw[y*16+x]=pix;
@@ -182,7 +180,7 @@ void CLCDDisplay::convert_data()
 #else
 void CLCDDisplay::convert_data ()
 {
-#ifndef HAVE_GENERIC_HARDWARE
+#ifndef PLATFORM_GENERIC
 	unsigned int x, y, z;
 	char tmp;
 
@@ -205,12 +203,13 @@ void CLCDDisplay::convert_data ()
 
 void CLCDDisplay::update()
 {
-#ifndef HAVE_GENERIC_HARDWARE
+#ifndef PLATFORM_GENERIC
 	convert_data();
 	if(paused || !available)
 		return;
 	else
-		if ( write(fd, lcd, LCD_BUFFER_SIZE) < 0) {
+		if ( write(fd, lcd, LCD_BUFFER_SIZE) < 0) 
+		{
 			perror("lcdd: CLCDDisplay::update(): write()");
 		}
 #endif
@@ -322,16 +321,17 @@ void CLCDDisplay::draw_line(const int x1, const int y1, const int x2, const int 
 	}
 }
 
-
-void CLCDDisplay::draw_fill_rect (int left,int top,int right,int bottom,int state) {
+void CLCDDisplay::draw_fill_rect (int left,int top,int right,int bottom,int state) 
+{
 	int x,y;
-	for(x = left + 1;x < right;x++) {  
-		for(y = top + 1;y < bottom;y++) {
+	for(x = left + 1;x < right;x++) 
+	{  
+		for(y = top + 1;y < bottom;y++) 
+		{
 			draw_point(x,y,state);
 		}
 	}
 }
-
 
 void CLCDDisplay::draw_rectangle (int left,int top, int right, int bottom, int linestate,int fillstate)
 {
@@ -345,7 +345,6 @@ void CLCDDisplay::draw_rectangle (int left,int top, int right, int bottom, int l
 	draw_fill_rect(left,top,right,bottom,fillstate);  
 }  
 
-
 void CLCDDisplay::draw_polygon(int num_vertices, int *vertices, int state) 
 {
 
@@ -353,7 +352,8 @@ void CLCDDisplay::draw_polygon(int num_vertices, int *vertices, int state)
 	// partly on screen)
 
 	int i;
-	for(i=0;i<num_vertices-1;i++) {
+	for(i=0;i<num_vertices-1;i++) 
+	{
 		draw_line(vertices[(i<<1)+0],
 			vertices[(i<<1)+1],
 			vertices[(i<<1)+2],
@@ -367,7 +367,6 @@ void CLCDDisplay::draw_polygon(int num_vertices, int *vertices, int state)
 		vertices[(num_vertices<<1)-1],
 		state);
 }
-
 
 struct rawHeader
 {
@@ -426,7 +425,8 @@ void CLCDDisplay::dump_screen(raw_display_t *screen) {
 	memmove(screen, raw, sizeof(raw_display_t));
 }
 
-void CLCDDisplay::load_screen(const raw_display_t * const screen) {
+void CLCDDisplay::load_screen(const raw_display_t * const screen) 
+{
 	memmove(raw, screen, sizeof(raw_display_t));
 }
 
