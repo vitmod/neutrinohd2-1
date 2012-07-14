9feef7f044727d4ba93e896dc037ad207a6352d0
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2012-07-14 13:48:48 +0000

    remove PVR build flag and code (libeplayer3 can do the same)

--- neutrino-hd/lib/libcoolstream/playback_cs.h
+++ neutrino-hd/lib/libcoolstream/playback_cs.h
@@ -26,36 +26,6 @@
 
 #include <config.h>
 
-#if defined (ENABLE_PVR)
-#define INBUF_SIZE (256 * 1024)
-
-typedef enum {
-	PLAYMODE_TS = 0,
-	PLAYMODE_FILE,
-} playmode_t;
-
-typedef enum {
-	FILETYPE_UNKNOWN,
-	FILETYPE_TS,
-	FILETYPE_MPG,
-	FILETYPE_VDR
-} filetype_t;
-
-/*
-typedef enum {
-	STATE_STOP,
-	STATE_PLAY,
-	STATE_PAUSE,
-	STATE_FF,
-	STATE_REW
-} playstate_t;
-*/
-
-typedef struct {
-	std::string Name;
-	off_t Size;
-} filelist_t;
-#endif
 
 #if defined (ENABLE_LIBEPLAYER3)
 #include <libeplayer3/include/common.h>
@@ -86,63 +56,9 @@ typedef enum {
 class cPlayback
 {
 	private:
-#if defined (ENABLE_PVR)		
-		uint8_t *inbuf;
-		ssize_t inbuf_pos;
-		ssize_t inbuf_sync;
-		uint8_t *pesbuf;
-		ssize_t pesbuf_pos;
-		ssize_t inbuf_read(void);
-		ssize_t read_ts(void);
-		ssize_t read_mpeg(void);
-
-		uint8_t cc[256];
-
-		int in_fd;
-
-		int video_type;
-		//int playback_speed;
-		
-		std::vector<filelist_t> filelist; /* for multi-file playback */
-
-		bool filelist_auto_add(void);
-		int mf_open(int fileno);
-		int mf_close(void);
-		off_t mf_lseek(off_t pos);
-		off_t mf_getsize(void);
-		int curr_fileno;
-		off_t curr_pos;
-		off_t bytes_per_second;
-
-		uint16_t vPid;
-		uint16_t aPid;
-		bool AC3;
-		uint16_t aPids[10];
-		unsigned short AC3flags[10];
-		std::string aLang[10];
-		uint16_t numPida;
-
-		int64_t pts_start;
-		int64_t pts_end;
-		int64_t pts_curr;
-		int64_t get_pts(uint8_t *p, bool pes);
-
-		filetype_t filetype;
-		//playstate_t playstate;
-
-		off_t seek_to_pts(int64_t pts);
-		off_t mp_seekSync(off_t pos);
-		int64_t get_PES_PTS(uint8_t *buf, int len, bool until_eof);
-
-		pthread_t thread;
-		bool thread_started;
-#endif
-
 #if defined (ENABLE_LIBEPLAYER3)
-		Context_t *player;
-#endif
-
-#if defined (ENABLE_GSTREAMER)
+		Context_t * player;
+#elif defined (ENABLE_GSTREAMER)
 		GstElement *m_gst_playbin;
 #endif
 		bool playing;
@@ -153,9 +69,6 @@ class cPlayback
 		int mAudioStream;
 
 	public:
-#if defined (ENABLE_PVR)	  
-		void playthread();
-#endif
 		bool Open();
 		void Close(void);
 		bool Start(char * filename, unsigned short vpid, int vtype, unsigned short apid, bool ac3);
