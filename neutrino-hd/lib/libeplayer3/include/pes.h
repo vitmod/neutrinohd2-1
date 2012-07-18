0a63e343248a2dd6c1d87a4b9e8521b9a9f4ec85
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2012-07-18 22:11:44 +0000

    clean up

--- neutrino-hd/lib/libeplayer3/include/pes.h
+++ neutrino-hd/lib/libeplayer3/include/pes.h
@@ -2,10 +2,10 @@
 #define pes_123
 
 #define PES_MAX_HEADER_SIZE                     64
-#define PES_PRIVATE_DATA_FLAG                           0x80
-#define PES_PRIVATE_DATA_LENGTH                         8
-#define PES_LENGTH_BYTE_0                               5
-#define PES_LENGTH_BYTE_1                               4
+#define PES_PRIVATE_DATA_FLAG                   0x80
+#define PES_PRIVATE_DATA_LENGTH                 8
+#define PES_LENGTH_BYTE_0                       5
+#define PES_LENGTH_BYTE_1                       4
 #define PES_FLAGS_BYTE                          7
 #define PES_EXTENSION_DATA_PRESENT              0x01
 #define PES_HEADER_DATA_LENGTH_BYTE             8
@@ -23,9 +23,9 @@
 #define H263_VIDEO_PES_START_CODE		0xfe
 #define H264_VIDEO_PES_START_CODE               0xe2
 #define MPEG_VIDEO_PES_START_CODE               0xe0
-#define MPEG_AUDIO_PES_START_CODE           0xc0
-#define VC1_VIDEO_PES_START_CODE	    0xfd
-#define AAC_AUDIO_PES_START_CODE            0xcf
+#define MPEG_AUDIO_PES_START_CODE           	0xc0
+#define VC1_VIDEO_PES_START_CODE	    	0xfd
+#define AAC_AUDIO_PES_START_CODE            	0xcf
 
 int InsertPesHeader (unsigned char *data, int size, unsigned char stream_id, unsigned long long int pts, int pic_start_code);
 int InsertVideoPrivateDataHeader(unsigned char *data, int payload_size);
