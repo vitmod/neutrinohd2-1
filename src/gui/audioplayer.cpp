8b6c473a5b804f94b07dc90b602f0f870d322271
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2012-08-14 10:09:26 +0000

    make it easy to build with sh4 project git (based on Tangocash patch)

--- src/gui/audioplayer.cpp
+++ src/gui/audioplayer.cpp
@@ -126,7 +126,7 @@ void CAudiofileExt::operator=(const CAudiofileExt& src)
 }
 
 #include <curl/curl.h>
-#include <curl/types.h>
+//#include <curl/types.h>
 #include <curl/easy.h>
 
 struct MemoryStruct {
