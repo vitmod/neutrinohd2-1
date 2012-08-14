8b1c84c3f672a21ae350c5a4cc1e934c6c98e7a7
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2012-08-14 10:06:11 +0000

    make it easy to build with sh4 project git (based on Tangocash patch)

--- branches/nhd2-exp/lib/libcoolstream/audio_cs.h
+++ branches/nhd2-exp/lib/libcoolstream/audio_cs.h
@@ -23,9 +23,13 @@
 
 #include <config.h>
 
-
+// stm_ioctl
 #ifdef __sh__
+#if defined (PLATFORM_DUCKBOX)
+#include <linux/dvb/stm_ioctls.h>
+#else
 #include <player2/linux/include/linux/dvb/stm_ioctls.h>
+#endif
 #endif
 
 
