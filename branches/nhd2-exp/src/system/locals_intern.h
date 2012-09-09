fb80c330455abb84915e1702624b3936849c7d7f
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2012-09-09 10:50:42 +0000

    typo

--- branches/nhd2-exp/src/system/locals_intern.h
+++ branches/nhd2-exp/src/system/locals_intern.h
@@ -1501,7 +1501,7 @@ const char *locale_real_names[] = {
 	"lcdmenu.menutitlevfd",
 	
 	"scansetup.tunermode",
-	"scansetup.tunermode_single",
+	"scansetup.tunermode_activ",
 	"scansetup.tunermode_twin",
 	"scansetup.tunermode_loop",
 	"scansetup.tunermode_notconnected",
