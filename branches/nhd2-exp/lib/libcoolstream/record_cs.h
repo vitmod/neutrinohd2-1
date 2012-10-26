91c27c0f87cff12eb87bd9b3f28dfc480947f5ae
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2012-10-26 14:44:18 +0000

    fix record

--- branches/nhd2-exp/lib/libcoolstream/record_cs.h
+++ branches/nhd2-exp/lib/libcoolstream/record_cs.h
@@ -1,9 +1,26 @@
+/*
+	License: GPL
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #ifndef __RECORD_TD_H
 #define __RECORD_TD_H
 
 #include <pthread.h>
 #include "dmx_cs.h"
-
 
 typedef enum {
 	RECORD_RUNNING,
@@ -17,25 +34,24 @@ typedef enum {
 class cRecord
 {
 	private:
-		int file_fd;
-		int dmx_num;
+		int demux_num;
 		int fe_num;
-		cDemux * dmx;
+		int file_fd;
+		cDemux *dmx;
 		pthread_t record_thread;
 		bool record_thread_running;
 		record_state_t exit_flag;
+		
 	public:
 		cRecord(int num = 0);
 		~cRecord();
 
 		bool Open();
-		bool Start(int fd, unsigned short vpid, unsigned short *apids, int numapids);
+		bool Start(int fd, unsigned short vpid, unsigned short *apids, int numpids);
 		bool Stop(void);
-		bool AddPid(unsigned short pid);
-		bool ChangePids(unsigned short vpid, unsigned short *apids, int numapids);
 
 		void RecordThread();
 };
 
 
-#endif
+#endif	/* record_cs.h*/
