832dd0d97cd78dc43cab6565b01b69c0ef679ac2
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2012-12-04 16:33:53 +0000

    clean up

--- branches/nhd2-exp/src/zapit/src/frontend.cpp
+++ branches/nhd2-exp/src/zapit/src/frontend.cpp
@@ -1142,9 +1142,6 @@ void CFrontend::positionMotor(uint8_t motorPosition)
 
 bool CFrontend::setInput(CZapitChannel * channel, bool nvod)
 {
-	//if(mode == (fe_mode_t)FE_NOTCONNECTED)
-	//	return false;
-	
 	transponder_list_t::iterator tpI;
 	transponder_id_t ct = channel->getTransponderId();
 
@@ -1258,9 +1255,6 @@ uint32_t CFrontend::sendEN50494TuningCommand(const uint32_t frequency, const int
 
 const bool CFrontend::tuneChannel(CZapitChannel * channel, bool nvod)
 {
-	//if(mode == (fe_mode_t)FE_NOTCONNECTED)
-	//	return false;
-	
 	dprintf(DEBUG_INFO, "CFrontend::tuneChannel: fe(%d,%d) tpid %llx\n", fe_adapter, fenumber, currentTransponder.TP_id);
 
 	transponder_list_t::iterator transponder = transponders.find(currentTransponder.TP_id);
