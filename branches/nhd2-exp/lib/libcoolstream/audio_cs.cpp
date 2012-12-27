5e8bbff704c8e55bc766b61d84c4ef19b3538234
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2012-12-27 13:12:35 +0000

    - add some volume/mute hacks from e2 (mipsel)

--- branches/nhd2-exp/lib/libcoolstream/audio_cs.cpp
+++ branches/nhd2-exp/lib/libcoolstream/audio_cs.cpp
@@ -130,6 +130,18 @@ int cAudio::SetMute(int enable)
 	
 	if(ret < 0)
 		perror("AUDIO_SET_MUTE");
+	
+#if !defined (PLATFORM_GENERIC)
+	//HACK?
+	FILE *f;
+	if((f = fopen("/proc/stb/audio/j1_mute", "wb")) == NULL) {
+		printf("cannot open /proc/stb/audio/j1_mute(%m)\n");
+	}
+	
+	fprintf(f, "%d", enable);
+
+	fclose(f);
+#endif 
 #endif
 
 	return ret;
@@ -174,6 +186,19 @@ int cAudio::setVolume(unsigned int left, unsigned int right)
 	
 	if(ret < 0)
 		perror("AUDIO_SET_MIXER");
+	
+#if !defined (PLATFORM_GENERIC)
+	//HACK?
+	FILE *f;
+	if((f = fopen("/proc/stb/avs/0/volume", "wb")) == NULL) 
+	{
+		printf("cannot open /proc/stb/avs/0/volume(%m)\n");
+	}
+
+	fprintf(f, "%d", left); /* in -1dB */
+
+	fclose(f);
+#endif // !generic
 #endif
 	
 	return ret;
