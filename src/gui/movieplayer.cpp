03a6ca8a6d03a6ccab8218a2efada7a4e81801db
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2013-03-30 10:59:57 +0000

    - clean up

--- src/gui/movieplayer.cpp
+++ src/gui/movieplayer.cpp
@@ -305,132 +305,129 @@ void CMovieInfoViewer::show(int Position)
 	// timescale
 	timescale->reset();
 	
-	if(!timeshift)
-	{
-		// paint shadow
-		frameBuffer->paintBoxRel(BoxStartX + SHADOW_OFFSET, BoxStartY + SHADOW_OFFSET, BoxWidth, BoxHeight, COL_INFOBAR_SHADOW_PLUS_0, RADIUS_MID, CORNER_TOP );
+	// paint shadow
+	frameBuffer->paintBoxRel(BoxStartX + SHADOW_OFFSET, BoxStartY + SHADOW_OFFSET, BoxWidth, BoxHeight, COL_INFOBAR_SHADOW_PLUS_0, RADIUS_MID, CORNER_TOP );
 		
-		// paint info box
-		frameBuffer->paintBoxRel(BoxStartX, BoxStartY, BoxWidth, BoxHeight, COL_INFOBAR_PLUS_0, RADIUS_MID, CORNER_TOP ); 
+	// paint info box
+	frameBuffer->paintBoxRel(BoxStartX, BoxStartY, BoxWidth, BoxHeight, COL_INFOBAR_PLUS_0, RADIUS_MID, CORNER_TOP ); 
 		
-		// timescale bg
-		frameBuffer->paintBoxRel(BoxStartX + 10, BoxStartY + 15, BoxWidth - 20, 6, COL_INFOBAR_SHADOW_PLUS_1 ); 
+	// timescale bg
+	frameBuffer->paintBoxRel(BoxStartX + 10, BoxStartY + 15, BoxWidth - 20, 6, COL_INFOBAR_SHADOW_PLUS_1 ); 
 		
-		// bottum bar
-		frameBuffer->paintBoxRel(BoxStartX, BoxStartY + (BoxHeight - 20), BoxWidth, 20, COL_INFOBAR_SHADOW_PLUS_1 ); 
+	// bottum bar
+	frameBuffer->paintBoxRel(BoxStartX, BoxStartY + (BoxHeight - 20), BoxWidth, 20, COL_INFOBAR_SHADOW_PLUS_1 ); 
 		
-		// mp icon
-		int m_icon_w = 0;
-		int m_icon_h = 0;
+	// mp icon
+	int m_icon_w = 0;
+	int m_icon_h = 0;
 		
-		frameBuffer->getIconSize("mp", &m_icon_w, &m_icon_h);
+	frameBuffer->getIconSize("mp", &m_icon_w, &m_icon_h);
 
-		int m_icon_x = BoxStartX + 5;
-		int m_icon_y = BoxStartY + (BoxHeight - m_icon_h) / 2;
-		frameBuffer->paintIcon("mp", m_icon_x, m_icon_y);
+	int m_icon_x = BoxStartX + 5;
+	int m_icon_y = BoxStartY + (BoxHeight - m_icon_h) / 2;
+	frameBuffer->paintIcon("mp", m_icon_x, m_icon_y);
 		
-		// paint buttons
-		// red
-		// movie info
-		frameBuffer->paintIcon(NEUTRINO_ICON_BUTTON_RED, BoxStartX + 2, BoxEndY - 18);
-		if( isMovieBrowser || isVlc )
-			g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_SMALL]->RenderString( BoxStartX + 2 + 16 + 2, BoxEndY + 2, BoxWidth/5, (char *)"Movie Info", (COL_INFOBAR_SHADOW + 1), 0, true); // UTF-8
+	// paint buttons
+	// red
+	// movie info
+	frameBuffer->paintIcon(NEUTRINO_ICON_BUTTON_RED, BoxStartX + 2, BoxEndY - 18);
+	if( isMovieBrowser || isVlc )
+		g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_SMALL]->RenderString( BoxStartX + 2 + 16 + 2, BoxEndY + 2, BoxWidth/5, (char *)"Movie Info", (COL_INFOBAR_SHADOW + 1), 0, true); // UTF-8
 		
-		// green
-		// audio
-		frameBuffer->paintIcon(NEUTRINO_ICON_BUTTON_GREEN, BoxStartX + BoxWidth/5, BoxEndY - 18);
-		g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_SMALL]->RenderString( BoxStartX + (BoxWidth/5)+ 18, BoxEndY + 2, BoxWidth/5, g_Locale->getText(LOCALE_INFOVIEWER_LANGUAGES), (COL_INFOBAR_SHADOW + 1), 0, true); // UTF-8
+	// green
+	// audio
+	frameBuffer->paintIcon(NEUTRINO_ICON_BUTTON_GREEN, BoxStartX + BoxWidth/5, BoxEndY - 18);
+	g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_SMALL]->RenderString( BoxStartX + (BoxWidth/5)+ 18, BoxEndY + 2, BoxWidth/5, g_Locale->getText(LOCALE_INFOVIEWER_LANGUAGES), (COL_INFOBAR_SHADOW + 1), 0, true); // UTF-8
 		
-		// yellow
-		// help
-		frameBuffer->paintIcon(NEUTRINO_ICON_BUTTON_YELLOW, BoxStartX + (BoxWidth/5)*2, BoxEndY - 18);
+	// yellow
+	// help
+	frameBuffer->paintIcon(NEUTRINO_ICON_BUTTON_YELLOW, BoxStartX + (BoxWidth/5)*2, BoxEndY - 18);
+	if( !isWebTV)
 		g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_SMALL]->RenderString( BoxStartX + (BoxWidth/5)*2 + 18, BoxEndY + 2, BoxWidth/5, (char *)"help", (COL_INFOBAR_SHADOW * 1), 0, true); // UTF-8
 		
-		// blue
-		// bookmark
-		frameBuffer->paintIcon(NEUTRINO_ICON_BUTTON_BLUE, BoxStartX + (BoxWidth/5)*3, BoxEndY - 18);
-		if(isMovieBrowser)
-			g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_SMALL]->RenderString( BoxStartX + (BoxWidth/5)*3 + 18, BoxEndY + 2, BoxWidth/5, g_Locale->getText(LOCALE_MOVIEPLAYER_BOOKMARK), (COL_INFOBAR_SHADOW + 1), 0, true); // UTF-8
+	// blue
+	// bookmark
+	frameBuffer->paintIcon(NEUTRINO_ICON_BUTTON_BLUE, BoxStartX + (BoxWidth/5)*3, BoxEndY - 18);
+	if(isMovieBrowser)
+		g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_SMALL]->RenderString( BoxStartX + (BoxWidth/5)*3 + 18, BoxEndY + 2, BoxWidth/5, g_Locale->getText(LOCALE_MOVIEPLAYER_BOOKMARK), (COL_INFOBAR_SHADOW + 1), 0, true); // UTF-8
 		
-		/* mp keys */
-		frameBuffer->paintIcon("ico_mp_rewind", BoxEndX - 60 - 16*5, BoxEndY - 18);
-		frameBuffer->paintIcon("ico_mp_play", BoxEndX - 60 - 16*4, BoxEndY - 18);
-		frameBuffer->paintIcon("ico_mp_pause", BoxEndX - 60 - 16*3, BoxEndY - 18);
-		frameBuffer->paintIcon("ico_mp_stop", BoxEndX - 60 - 16*2, BoxEndY - 18);
-		frameBuffer->paintIcon("ico_mp_forward", BoxEndX - 60 - 16, BoxEndY - 18);
+	/* mp keys */
+	frameBuffer->paintIcon("ico_mp_rewind", BoxEndX - 60 - 16*5, BoxEndY - 18);
+	frameBuffer->paintIcon("ico_mp_play", BoxEndX - 60 - 16*4, BoxEndY - 18);
+	frameBuffer->paintIcon("ico_mp_pause", BoxEndX - 60 - 16*3, BoxEndY - 18);
+	frameBuffer->paintIcon("ico_mp_stop", BoxEndX - 60 - 16*2, BoxEndY - 18);
+	frameBuffer->paintIcon("ico_mp_forward", BoxEndX - 60 - 16, BoxEndY - 18);
 		
-		// ac3
-		frameBuffer->paintIcon( (ac3state == CInfoViewer::AC3_ACTIVE)?NEUTRINO_ICON_DD:NEUTRINO_ICON_DD_GREY, BoxEndX - 2 - 26, BoxEndY - 18);
+	// ac3
+	frameBuffer->paintIcon( (ac3state == CInfoViewer::AC3_ACTIVE)?NEUTRINO_ICON_DD:NEUTRINO_ICON_DD_GREY, BoxEndX - 2 - 26, BoxEndY - 18);
 		
-		// 4:3/16:9
-		const char * aspect_icon = NEUTRINO_ICON_16_9_GREY;
+	// 4:3/16:9
+	const char * aspect_icon = NEUTRINO_ICON_16_9_GREY;
 				
-		if(videoDecoder->getAspectRatio() == 1)
-			aspect_icon = NEUTRINO_ICON_16_9;
+	if(videoDecoder->getAspectRatio() == 1)
+		aspect_icon = NEUTRINO_ICON_16_9;
 				
-		frameBuffer->paintIcon(aspect_icon, BoxEndX - 2 - 55, BoxEndY - 18);
+	frameBuffer->paintIcon(aspect_icon, BoxEndX - 2 - 55, BoxEndY - 18);
 		
-		//playstate
-		const char *icon = "mp_play";
+	//playstate
+	const char *icon = "mp_play";
 		
-		switch(playstate)
-		{
-			case CMoviePlayerGui::PAUSE: icon = "mp_pause"; break;
-			case CMoviePlayerGui::PLAY: icon = "mp_play"; break;
-			case CMoviePlayerGui::REW: icon = "mp_b-skip"; break;
-			case CMoviePlayerGui::FF: icon = "mp_f-skip"; break;
-			case CMoviePlayerGui::SOFTRESET: break;
-			case CMoviePlayerGui::SLOW: break;
-			case CMoviePlayerGui::STOPPED: break;
-		}
-
-		// get icon size
-		int icon_w = 0;
-		int icon_h = 0;
-		
-		frameBuffer->getIconSize(icon, &icon_w, &icon_h);
-
-		//int icon_x = BoxStartX + 60 + 5;
-		int icon_x = BoxStartX + 5 + m_icon_w + 10;
-		int icon_y = BoxStartY + (BoxHeight - icon_h) / 2;
-		
-
-		frameBuffer->paintIcon(icon, icon_x, icon_y);
-		
-		// paint speed
-		char strSpeed[4];
-		if( playstate == CMoviePlayerGui::FF || playstate == CMoviePlayerGui::REW )
-		{
-			sprintf(strSpeed, "%d", speed);
-			
-			g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_NUMBER]->RenderString(icon_x + icon_w + 5, BoxStartY + (BoxHeight/3)*2, BoxWidth/5, strSpeed, COL_COLORED_EVENTS_INFOBAR ); // UTF-8
-		}
-		
-		// infos
-		
-		// duration
-		char runningTotal[32]; // %d can be 10 digits max...	
-		sprintf(runningTotal, "%d / %d min", Position/60, (duration + 30000) / 60000 );	
-		//sprintf(runningTotal, "%d min", (duration + 30000) / 60000 );	
-		
-		int durationWidth = g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO]->getRenderWidth(runningTotal);
-		int durationTextPos = BoxEndX - durationWidth - 15;
-		
-		int speedWidth = g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO]->getRenderWidth("-8");
-		
-		int InfoStartX = BoxStartX + 5 + m_icon_w + 10 + icon_w + 5 + speedWidth + 20;
-		int InfoWidth = durationTextPos - InfoStartX;
-		
-		//Title 1
-		g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO]->RenderString (InfoStartX, BoxStartY + BoxHeight/2 - 5, InfoWidth, g_file_epg, COL_INFOBAR, 0, true);
-
-		//Title2
-		g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO]->RenderString (InfoStartX, BoxStartY + BoxHeight/2 + 25, InfoWidth, g_file_epg1, COL_INFOBAR, 0, true);
-
-		// duration
-		if(!isWebTV && !isVlc)
-			g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO]->RenderString(durationTextPos, BoxStartY + BoxHeight/2 - 5, durationWidth, runningTotal, COL_INFOBAR);
+	switch(playstate)
+	{
+		case CMoviePlayerGui::PAUSE: icon = "mp_pause"; break;
+		case CMoviePlayerGui::PLAY: icon = "mp_play"; break;
+		case CMoviePlayerGui::REW: icon = "mp_b-skip"; break;
+		case CMoviePlayerGui::FF: icon = "mp_f-skip"; break;
+		case CMoviePlayerGui::SOFTRESET: break;
+		case CMoviePlayerGui::SLOW: break;
+		case CMoviePlayerGui::STOPPED: break;
 	}
+
+	// get icon size
+	int icon_w = 0;
+	int icon_h = 0;
+		
+	frameBuffer->getIconSize(icon, &icon_w, &icon_h);
+
+	//int icon_x = BoxStartX + 60 + 5;
+	int icon_x = BoxStartX + 5 + m_icon_w + 10;
+	int icon_y = BoxStartY + (BoxHeight - icon_h) / 2;
+		
+
+	frameBuffer->paintIcon(icon, icon_x, icon_y);
+		
+	// paint speed
+	char strSpeed[4];
+	if( playstate == CMoviePlayerGui::FF || playstate == CMoviePlayerGui::REW )
+	{
+		sprintf(strSpeed, "%d", speed);
+			
+		g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_NUMBER]->RenderString(icon_x + icon_w + 5, BoxStartY + (BoxHeight/3)*2, BoxWidth/5, strSpeed, COL_COLORED_EVENTS_INFOBAR ); // UTF-8
+	}
+		
+	// infos
+	// duration
+	char runningTotal[32]; // %d can be 10 digits max...	
+	sprintf(runningTotal, "%d / %d min", Position/60, (duration + 30000) / 60000 );	
+	//sprintf(runningTotal, "%d min", (duration + 30000) / 60000 );	
+		
+	int durationWidth = g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO]->getRenderWidth(runningTotal);
+	int durationTextPos = BoxEndX - durationWidth - 15;
+		
+	int speedWidth = g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO]->getRenderWidth("-8");
+		
+	int InfoStartX = BoxStartX + 5 + m_icon_w + 10 + icon_w + 5 + speedWidth + 20;
+	int InfoWidth = durationTextPos - InfoStartX;
+		
+	//Title 1
+	g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO]->RenderString (InfoStartX, BoxStartY + BoxHeight/2 - 5, InfoWidth, g_file_epg, COL_INFOBAR, 0, true);
+
+	//Title2
+	g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO]->RenderString (InfoStartX, BoxStartY + BoxHeight/2 + 25, InfoWidth, g_file_epg1, COL_INFOBAR, 0, true);
+
+	// duration
+	if(!isWebTV && !isVlc)
+		g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_INFO]->RenderString(durationTextPos, BoxStartY + BoxHeight/2 - 5, durationWidth, runningTotal, COL_INFOBAR);
 	
 	updatePos(file_prozent);
 
