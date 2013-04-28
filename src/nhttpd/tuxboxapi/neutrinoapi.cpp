14a76534c0d02355dfe9fe23e897e1146a46cd97
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2013-04-28 12:12:19 +0000

    - minor fix

--- src/nhttpd/tuxboxapi/neutrinoapi.cpp
+++ src/nhttpd/tuxboxapi/neutrinoapi.cpp
@@ -215,13 +215,11 @@ void CNeutrinoAPI::ZapToChannelId(t_channel_id channel_id)
 	}
 	else
 	{
-		if (channel_id == Zapit->getCurrentServiceID())
-		{
-			//printf("Kanal ist aktuell\n");
+		if ( channel_id == Zapit->getCurrentServiceID() )
 			return;
-		}
 
-		if (Zapit->zapTo_serviceID(channel_id) != CZapitClient::ZAP_INVALID_PARAM)
+		//if (Zapit->zapTo_serviceID(channel_id) != CZapitClient::ZAP_INVALID_PARAM)
+		if (Zapit->zapTo_record(channel_id) != CZapitClient::ZAP_INVALID_PARAM)
 			Sectionsd->setServiceChanged(channel_id&0xFFFFFFFFFFFFULL, false);
 	}
 }
