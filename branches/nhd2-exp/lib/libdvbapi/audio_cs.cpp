99523b35287b6b0dcf9ac44a3bf3754dc96e22f8
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2013-06-10 13:10:45 +0000

    [libdvbapi] -fix audio volume for some mipsel platforms when playing movies (dirty) - [spark7162] - fix VFD (patch by DboxOldie)

--- branches/nhd2-exp/lib/libdvbapi/audio_cs.cpp
+++ branches/nhd2-exp/lib/libdvbapi/audio_cs.cpp
@@ -179,6 +179,20 @@ int cAudio::setVolume(unsigned int left, unsigned int right)
 		if(ret < 0)
 			perror("AUDIO_SET_MIXER");
 	}
+
+#if !defined (PLATFORM_GENERIC) && defined (PLATFORM_HYPERCUBE)
+ 	//HACK?
+ 	FILE *f;
+ 
+ 	if((f = fopen("/proc/stb/avs/0/volume", "wb")) == NULL)
+ 	{
+ 		printf("cannot open /proc/stb/avs/0/volume(%m)\n");
+ 	}
+ 
+ 	fprintf(f, "%d", volume); /* in -1dB */
+ 
+ 	fclose(f); 
+#endif	
 #endif
 	
 	return ret;
