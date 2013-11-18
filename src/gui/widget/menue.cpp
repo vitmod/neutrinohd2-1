5af312d49a3c69aea42e761e5c5b0906a19b9b4e
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2013-11-18 09:37:49 +0000

    [features] -extended

--- src/gui/widget/menue.cpp
+++ src/gui/widget/menue.cpp
@@ -1810,18 +1810,18 @@ int CMenuForwarderItemMenuIcon::paint(bool selected)
 			/* get icon size */
 			int icon_w = 0;
 			int icon_h = 0;
+			int hheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU_TITLE]->getHeight();
+			int fheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU_TITLE]->getHeight();
+			int sp_height = 5;
 			
 			// check item icon size w/h equal 0 means icons doesnt exist
 			frameBuffer->getIconSize(itemIcon.c_str(), &icon_w, &icon_h);
 			
 			//refresh pic box
-			frameBuffer->paintBoxRel(x + BORDER_LEFT + (dx/3)*2 + (((dx - (dx/3)*2 - BORDER_RIGHT)/2) - ITEM_ICON_W/2), ( frameBuffer->getScreenHeight(true) - ITEM_ICON_H)/2, ITEM_ICON_W, ITEM_ICON_H, COL_MENUCONTENTDARK_PLUS_0 ); // 25 foot height
+			if( ((FULL_HEIGHT - (hheight + fheight + 2*sp_height)) >= (ITEM_ICON_H) ) && (icon_w >= 100 || icon_h >= 100) )
+				frameBuffer->paintBoxRel(x + BORDER_LEFT + (dx/3)*2 + (((dx - (dx/3)*2 - BORDER_RIGHT)/2) - ITEM_ICON_W/2), ( frameBuffer->getScreenHeight(true) - ITEM_ICON_H)/2, ITEM_ICON_W, ITEM_ICON_H, COL_MENUCONTENTDARK_PLUS_0 ); // 25 foot height
 		
 			// paint item icon
-			int hheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU_TITLE]->getHeight();
-			int fheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU_TITLE]->getHeight();
-			int sp_height = 5;
-	
 			if( ((FULL_HEIGHT - (hheight + fheight + 2*sp_height)) >= (ITEM_ICON_H) ) && (icon_w >= 100 || icon_h >= 100) )
 				frameBuffer->paintIcon(itemIcon.c_str(), x + BORDER_LEFT + (dx/3)*2 + ((( dx - (dx/3)*2 - BORDER_RIGHT)/2) - icon_w/2), ( frameBuffer->getScreenHeight(true) - icon_h)/2);  //25:foot height
 		}
