a37ebd84be647ec32094a7a724528620b7686e55
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2013-12-12 14:38:22 +0000

    [test] -other test demo

--- test/test.cpp
+++ test/test.cpp
@@ -8,6 +8,15 @@ int plugin_exec(void)
 {
 	printf("Plugins: starting test\n");
 	
-	ShowMsg2UTF("neutrino", "to be continued", CMsgBox::mbrBack, CMsgBox::mbBack);
+	//ShowMsg2UTF("neutrino", "to be continued", CMsgBox::mbrBack, CMsgBox::mbBack);
+	CFileBrowser filebrowser;
+	filebrowser.use_filter = false;	
+	if(filebrowser.exec("/media/hdd"))
+	{
+		g_settings.streaming_server_url = filebrowser.getSelectedFile()->Name.c_str();
+		moviePlayerGui->exec(NULL, "urlplayback");
+	}
+	
+	return 0;
 }
 
