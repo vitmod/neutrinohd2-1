0f368927941dece436b48f546449d26f347b5ce8
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2014-03-09 16:05:08 +0000

    - add simply mediaportal plugin

--- mediaportal/mediaportal.cpp
+++ mediaportal/mediaportal.cpp
@@ -26,42 +26,76 @@
 
 
 extern "C" int plugin_exec(void);
-  
+
+class CMediaPortal : public CMenuTarget
+{
+        public:
+                int exec(CMenuTarget *parent,  const std::string &actionkey);
+};
+
+int CMediaPortal::exec(CMenuTarget *parent, const std::string &actionKey)
+{
+	if(parent)
+		parent->hide();
+
+	printf("CMediaPortal::exec: %s\n", actionKey.c_str());
+	
+	if(actionKey == "youtube") 
+	{
+		moviePlayerGui->exec(NULL, "ytplayback");
+	}
+	else if(actionKey == "netzkino") 
+	{
+		moviePlayerGui->exec(NULL, "netzkinoplayback");
+	}
+	else if(actionKey == "musicdeluxe")
+	{
+		moviePlayerGui->filename = "rtmp://flash.cdn.deluxemusic.tv/deluxemusic.tv-live/web_850.stream";
+		moviePlayerGui->exec(NULL, "urlplayback");
+	}
+	else if(actionKey == "orf1")
+	{
+		moviePlayerGui->filename = "rtsp://apasfwl.apa.at:80/orf1_q6a/orf.sdp";	
+		moviePlayerGui->exec(NULL, "urlplayback");
+	}
+	else if(actionKey == "orf2")
+	{
+		moviePlayerGui->filename = "rtsp://apasfwl.apa.at:80/orf2_q6a/orf.sdp";	
+		moviePlayerGui->exec(NULL, "urlplayback");
+	}
+	else if(actionKey == "orf3")
+	{
+		moviePlayerGui->filename = "rtsp://apasfwl.apa.at:80/orf3_q6a/orf.sdp";	
+		moviePlayerGui->exec(NULL, "urlplayback");
+	}
+	else if(actionKey == "orfsport")
+	{
+		moviePlayerGui->filename = "rtsp://apasfwl.apa.at/orfs_q6a/orf.sdp";	
+		moviePlayerGui->exec(NULL, "urlplayback");
+	}
+
+	return menu_return::RETURN_REPAINT;
+}
+
 int plugin_exec(void)
 {
 	printf("Plugins: starting Media Portal\n");
 	
-	CMenuWidget * mediaPortal = new CMenuWidget("Media Portal", NEUTRINO_ICON_STREAMING);
+	CMenuWidget * mediaPortal = new CMenuWidget("Media Portal",NEUTRINO_ICON_BUTTON_SETUP);
+	CMediaPortal * mpHandler = new CMediaPortal();
 	
-	// yt
-	mediaPortal->addItem(new CMenuForwarderItemMenuIconNonLocalized("YouTube", true, NULL, moviePlayerGui, "ytplayback", CRCInput::convertDigitToKey(1)));
-	
-	// netzkino
-	mediaPortal->addItem(new CMenuForwarderItemMenuIconNonLocalized("NetzKino.de", true, NULL, moviePlayerGui, "netzkinoplayback", CRCInput::convertDigitToKey(2)));	
-	
-	// deluxe music
-	moviePlayerGui->filename = "rtmp://flash.cdn.deluxemusic.tv/deluxemusic.tv-live/web_850.stream";	
-	mediaPortal->addItem(new CMenuForwarderItemMenuIconNonLocalized("Music Deluxe", true, NULL, moviePlayerGui, "urlplayback", CRCInput::convertDigitToKey(3)));	
-	
-	// orf1
-	moviePlayerGui->filename = "rtsp://apasfwl.apa.at:80/orf1_q6a/orf.sdp";	
-	mediaPortal->addItem(new CMenuForwarderItemMenuIconNonLocalized("ORF 1", true, NULL, moviePlayerGui, "urlplayback", CRCInput::convertDigitToKey(4)));
-	
-	// orf2
-	moviePlayerGui->filename = "rtsp://apasfwl.apa.at:80/orf2_q6a/orf.sdp";	
-	mediaPortal->addItem(new CMenuForwarderItemMenuIconNonLocalized("ORF 2", true, NULL, moviePlayerGui, "urlplayback", CRCInput::convertDigitToKey(3)));
-	
-	// orf3
-	moviePlayerGui->filename = "rtsp://apasfwl.apa.at:80/orf3_q6a/orf.sdp";	
-	mediaPortal->addItem(new CMenuForwarderItemMenuIconNonLocalized("ORF 3", true, NULL, moviePlayerGui, "urlplayback", CRCInput::convertDigitToKey(3)));
-	
-	// orf sport hq
-	moviePlayerGui->filename = "rtsp://apasfwl.apa.at/orfs_q6a/orf.sdp";	
-	mediaPortal->addItem(new CMenuForwarderItemMenuIconNonLocalized("ORF Sport HQ", true, NULL, moviePlayerGui, "urlplayback", CRCInput::convertDigitToKey(3)));
+	mediaPortal->addItem(new CMenuForwarderNonLocalized("Youtube", true, NULL, mpHandler, "youtube"));
+	mediaPortal->addItem(new CMenuForwarderNonLocalized("Netzkino.de", true, NULL, mpHandler, "netzkino"));
+	mediaPortal->addItem(new CMenuForwarderNonLocalized("Music Deluxe", true, NULL, mpHandler, "musicdeluxe"));
+	mediaPortal->addItem(new CMenuForwarderNonLocalized("ORF 1", true, NULL, mpHandler, "orf1"));
+	mediaPortal->addItem(new CMenuForwarderNonLocalized("ORF 2", true, NULL, mpHandler, "orf2"));
+	mediaPortal->addItem(new CMenuForwarderNonLocalized("ORF 3", true, NULL, mpHandler, "orf3"));
+	mediaPortal->addItem(new CMenuForwarderNonLocalized("ORF Sport HQ", true, NULL, mpHandler, "orfsport"));
 	
 	mediaPortal->exec(NULL, "");
 	mediaPortal->hide();
 	
+	delete mpHandler;
 	delete mediaPortal;
 	
 	return 0;
