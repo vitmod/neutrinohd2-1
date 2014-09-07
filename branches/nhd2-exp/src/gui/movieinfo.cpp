f732e5a32443ad716ed2781422a8da94b55fd808
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2014-09-07 22:20:56 +0000

    yt/nk show publication date in movie info.

--- branches/nhd2-exp/src/gui/movieinfo.cpp
+++ branches/nhd2-exp/src/gui/movieinfo.cpp
@@ -524,17 +524,25 @@ void CMovieInfo::showMovieInfo(MI_MOVIE_INFO & movie_info)
 	}
 
 	print_buffer += "\n\n";
-	print_buffer += g_Locale->getText(LOCALE_MOVIEBROWSER_INFO_PREVPLAYDATE);
-	print_buffer += ": ";
-	date_tm = localtime(&movie_info.dateOfLastPlay);
-	snprintf(date_char, 12, "%02d.%02d.%04d", date_tm->tm_mday, date_tm->tm_mon + 1, date_tm->tm_year + 1900);
-	print_buffer += date_char;
-	print_buffer += "\n";
-	print_buffer += g_Locale->getText(LOCALE_MOVIEBROWSER_INFO_RECORDDATE);
-	print_buffer += ": ";
-	date_tm = localtime(&movie_info.file.Time);
-	snprintf(date_char, 12, "%02d.%02d.%04d", date_tm->tm_mday, date_tm->tm_mon + 1, date_tm->tm_year + 1900);
-	print_buffer += date_char;
+	
+	if(!movie_info.ytdate.empty())
+	{
+		print_buffer += movie_info.ytdate;
+	}
+	else
+	{
+		print_buffer += g_Locale->getText(LOCALE_MOVIEBROWSER_INFO_PREVPLAYDATE);
+		print_buffer += ": ";
+		date_tm = localtime(&movie_info.dateOfLastPlay);
+		snprintf(date_char, 12, "%02d.%02d.%04d", date_tm->tm_mday, date_tm->tm_mon + 1, date_tm->tm_year + 1900);
+		print_buffer += date_char;
+		print_buffer += "\n";
+		print_buffer += g_Locale->getText(LOCALE_MOVIEBROWSER_INFO_RECORDDATE);
+		print_buffer += ": ";
+		date_tm = localtime(&movie_info.file.Time);
+		snprintf(date_char, 12, "%02d.%02d.%04d", date_tm->tm_mday, date_tm->tm_mon + 1, date_tm->tm_year + 1900);
+		print_buffer += date_char;
+	}
 	
 	if (movie_info.file.Size != 0) 
 	{
