eda81f16db53bf73131db01120a7e5f0e17b0e36
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2014-09-08 22:51:16 +0000

    -fix seektrick (gstreamer)

--- branches/nhd2-exp/lib/libdvbapi/playback_cs.cpp
+++ branches/nhd2-exp/lib/libdvbapi/playback_cs.cpp
@@ -811,6 +811,15 @@ bool cPlayback::GetPosition(int64_t &position, int64_t &duration)
 		gint64 pts;
 		position = 0;
 		
+		/*
+		if(audioSink || videoSink)
+		{
+			g_signal_emit_by_name(audioSink ? audioSink : videoSink, "get-decoder-time", &pts);
+			GST_CLOCK_TIME_IS_VALID(pts);
+		}
+		*/
+
+		/*
 		if(audioSink && !isTS)
 		{
 			gchar *name = gst_element_get_name(audioSink);
@@ -837,8 +846,8 @@ bool cPlayback::GetPosition(int64_t &position, int64_t &duration)
 			else
 				gst_element_query_position(m_gst_playbin, &fmt, &pts);
 		}
-		else		  
-			gst_element_query_position(m_gst_playbin, &fmt, &pts);
+		*/		  
+		gst_element_query_position(m_gst_playbin, &fmt, &pts);
 			
 		position = pts / 1000000;	// in ms
 		
