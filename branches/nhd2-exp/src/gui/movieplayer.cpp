e80fef6214ad4842274682568552ac8e5bf9465a
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2015-06-30 23:01:37 +0000

    movieplayer: fix movie infos for urlplayback

--- branches/nhd2-exp/src/gui/movieplayer.cpp
+++ branches/nhd2-exp/src/gui/movieplayer.cpp
@@ -622,14 +622,8 @@ void CMoviePlayerGui::PlayFile(void)
 		}
 		else if(filename != NULL)
 		{
-			sel_filename = std::string(rindex(filename, '/') + 1);
-			
-			if(Title.empty())
-				Title = sel_filename;
-			if(Info1.empty())
-				Info1 = sel_filename;
-			if(Info2.empty())
-				Info2 = sel_filename;
+			if(!Title.empty())
+				sel_filename = Title;
 			
 			//thumbnail
 			if(thumbnail.empty())
@@ -2163,9 +2157,4 @@ void CMoviePlayerGui::showFileInfo()
 	msgBox->setText(&buffer, thumbnail, lx, ly, picw, pich);
 	msgBox->exec();
 	delete msgBox;
-	
-	/*
-	if (p_movie_info != NULL)
-                cMovieInfo.showMovieInfo(*p_movie_info);
-	*/
 }
