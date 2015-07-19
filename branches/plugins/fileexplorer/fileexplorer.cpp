dc76d98158f11a4960fa4fc6739cb942bf6e6267
mohousch <mohousch@2c6f6a7f-2313-61a7-ac6f-8ad46624f247>
2015-07-19 14:01:39 +0000

    [plugins]: minor fixes

--- branches/plugins/fileexplorer/fileexplorer.cpp
+++ branches/plugins/fileexplorer/fileexplorer.cpp
@@ -80,17 +80,18 @@ BROWSER:
 			else if(file->getType() == CFile::FILE_TEXT || file->getType() == CFile::FILE_XML)
 			{
 				std::string buffer;
+				buffer.clear();
 				
-				/*
+				char buf[256];
 				FILE* pFile;
 				pFile = fopen(file->Name.c_str(), "r");
-				if(pFile)
+				if(pFile != NULL)
 				{
-					fgets((char *)buffer.c_str(), 256, pFile); 
+					fgets(buf, sizeof(buf), pFile);
 				}
 				fclose(pFile);
-				*/
 
+				buffer = buf;
 				int mode =  CInfoBox::SCROLL | CInfoBox::TITLE | CInfoBox::FOOT | CInfoBox::BORDER;// | //CInfoBox::NO_AUTO_LINEBREAK | //CInfoBox::CENTER | //CInfoBox::AUTO_WIDTH | //CInfoBox::AUTO_HIGH;
 				CBox position(g_settings.screen_StartX + 50, g_settings.screen_StartY + 50, g_settings.screen_EndX - g_settings.screen_StartX - 100, g_settings.screen_EndY - g_settings.screen_StartY - 100); 
 					
@@ -98,7 +99,6 @@ BROWSER:
 				infoBox->setText(&buffer);
 				infoBox->exec();
 				delete infoBox;
-				buffer.clear();
 			}
 			else if(file->getType() == CFile::FILE_VIDEO)
 			{
